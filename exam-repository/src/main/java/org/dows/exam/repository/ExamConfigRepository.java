18a12171faf29637a102aa09ec9719995396d047
AutoGen <auto@builder>
2025-11-30 22:15:15 +0800

    auto commit by generator

--- exam-repository/src/main/java/org/dows/exam/repository/ExamConfigRepository.java
+++ exam-repository/src/main/java/org/dows/exam/repository/ExamConfigRepository.java
@@ -1,7 +1,6 @@
 package org.dows.exam.repository;
 
 import org.dows.rade.crud.CrudRepository;
-import org.dows.rongz.dao.TaskConfigDao;
 import org.springframework.stereotype.Component;
 
 import org.dows.exam.entity.ExamConfigEntity;
